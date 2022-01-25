f2ff2a6509c92778bb3f3fdac26be4065e5d01a5
handmadeCode <sbae5617@gmail.com>
2022-01-25 21:37:22 +0900

    Add ws chat

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
-import res from "express/lib/response";
 
 const app = express();
 
@@ -7,7 +8,32 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "ㅇㅇ";
+    console.log("Connected to Server✅");
+    socket.on("close", () => console.log("Disconnected from Browser❌"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg.toString("utf8"));
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach(aSocket =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        };
+    });
+});
+
+server.listen(3000, handleListen);
